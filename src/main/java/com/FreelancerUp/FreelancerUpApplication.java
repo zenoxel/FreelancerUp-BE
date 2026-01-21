dbb47caabd4586b05f3639adcbe655b50d3e7eed
The Ngoc <nthengoc.dev@gmail.com>
2026-01-21 15:55:17 +0700

    Debug (#13)
    * fix: syntax
    
    * fix: env

--- src/main/java/com/FreelancerUp/FreelancerUpApplication.java
+++ src/main/java/com/FreelancerUp/FreelancerUpApplication.java
@@ -1,5 +1,6 @@
 package com.FreelancerUp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FreelancerUpApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMalformed()
+				.ignoreIfMissing()
+				.load();
+
+		// Set system properties from .env
+		dotenv.entries().forEach(entry ->
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(FreelancerUpApplication.class, args);
 	}
 
